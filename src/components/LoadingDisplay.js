8c08fe4dc3b127a495f6eac86e3905433d30b9fe
Ethanleba <ethanleba5@gmail.com>
2019-07-31 15:02:58 -0400

    animated big text

--- src/components/LoadingDisplay.js
+++ src/components/LoadingDisplay.js
@@ -11,24 +11,28 @@ const loadList = {
 // Draws the component shown to the user while waiting for the page to load
 const LoadingDisplay = () => {
   const [current, setCurrent] = useState(0);
+  const [message, setMessage] = useState(false);
   const [submessage, setSubmessage] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent(current => (current + 1) % 4)
     }, 600);
-    const timer = setTimeout(() =>
-    setSubmessage(_ => true), 3000);
+    const submsgTimer = setTimeout(() =>
+    setSubmessage(_ => true), 4000);
+    const msgTimer = setTimeout(() =>
+    setMessage(_ => true), 1000);
     return () => {
       clearInterval(interval);
-      clearTimeout(timer)
+      clearTimeout(msgTimer);
+      clearTimeout(submsgTimer);
     };
   }, []);
 
   return (
     <div className="d-flex flex-column h-100 justify-content-center">
-    <h2 className="w-100 text-center">{loadList[current]}</h2>
-    {submessage ? ( <p className={`text-center text-muted ${style.loadingText}`}>The Heroku servers are warming up!</p> ) :
+    {message && <h2 className={`w-100 text-center ${style.msgAnimation}`}>{loadList[current]}</h2>}
+    {submessage ? ( <p className={`text-center text-muted ${style.submsgAnimation}`}>The Heroku servers are warming up!</p> ) :
     (<p>&nbsp;</p>)}
     </div>
   )
