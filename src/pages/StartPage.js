282f9259ce6c02c0afcbfdc43d90378dc90329ae
Ethanleba <ethanleba5@gmail.com>
2019-08-04 12:15:14 -0400

    Factored out startpage.css into bootstrap

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -2,21 +2,20 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 import "./Page.css";
-import "./StartPage.css";
 
 // The initial page that the user sees when they load the website
 class StartPage extends React.Component {
   render() {
     return (
-      <div id="main">
+      <div className="d-flex flex-column justify-content-around align-items-center h-100">
           <div>
-          <h1 className="font-weight-bold">Transcriboid</h1>
-          <p className="m-0 text-center font-weight-normal">A randomly generated transcription trainer.</p>
+          <h1 className="display-2 font-weight-bold">Transcriboid</h1>
+          <p className="m-0 text-center font-weight-bold text-muted">A randomly generated transcription trainer.</p>
           </div>
           <Link to="/main">
             <button className="large">Start</button>
           </Link>
-        <p className="madetag">Made by Ethan Leba, 2019</p>
+        <p className="m-3 fixed-bottom text-right">Made by Ethan Leba, 2019</p>
       </div>
     );
   }
